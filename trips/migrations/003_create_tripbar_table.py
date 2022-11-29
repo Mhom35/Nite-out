73be3e08c04f78d4d3936961dc63c7eee524bc15
Derrick Wan <drrckwan.s@gmail.com>
2022-11-29 02:02:30 +0000

    removed yelp from yaml postgres, added positions to tripbar table, barout,...

--- trips/migrations/003_create_tripbar_table.py
+++ trips/migrations/003_create_tripbar_table.py
@@ -1,10 +1,11 @@
-steps =[
+steps = [
     [
         ## Create the table
         """
         CREATE TABLE trip_bars (
             trip_id INT NOT NULL,
             bar_id INT NOT NULL,
+            positions INT NOT NULL,
             PRIMARY KEY (trip_id, bar_id)
         );
         """,
--- trips/routers/tripBars.py
+++ trips/routers/tripBars.py
@@ -3,13 +3,15 @@ from typing import List
 from typing import List, Union, Optional
 
 
-from queries.trip_bars import (TripBarRepository, TripBarOut, TripBarIn)
+from queries.trip_bars import TripBarRepository, TripBarOut, TripBarIn
+from queries.trips import TripIn
 
 router = APIRouter()
 
+
 @router.post("/middletable", response_model=Optional[TripBarOut])
 def create_middletable(
-    tripBar:TripBarIn,
+    tripBar: TripBarIn,
     response: Response,
     repo: TripBarRepository = Depends(),
 ):
@@ -17,3 +19,23 @@ def create_middletable(
         return repo.create_trip_bar(tripBar)
     except Exception:
         response.status_code = 400
+
+
+@router.delete(
+    "/trips/{trip_id}/delete-bar-connection/{bar_id}", response_model=bool
+)
+def delete_bar_from_trip(
+    trip_id: int,
+    bar_id: int,
+    repo: TripBarRepository = Depends(),
+) -> bool:
+    return repo.delete_bar_from_trip(trip_id, bar_id)
+
+
+@router.put("/trips/{trip_id}/update-bar")
+def update_bars_for_trips(
+    trip_id: int,
+    trip_in: TripIn,
+    repo: TripBarRepository = Depends(),
+):
+    return repo.update_bars_for_trips(trip_id, trip_in.locations)
