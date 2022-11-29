9efd18c2127ad77afc727740d9d0027b89f86573
Mitchell <mitchell.hom35@gmail.com>
2022-11-29 15:33:07 -0800

    added CORS allowed origins need to import CORSmiddleware

--- trips/main.py
+++ trips/main.py
@@ -3,6 +3,19 @@ from routers import trips, bars,tripBars
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(trips.router)
 app.include_router(bars.router)
 app.include_router(tripBars.router)
