044539b530eb1f07ab6f16bc77565137908ebe2d
Mitchell <mitchell.hom35@gmail.com>
2022-11-29 15:28:36 -0800

    deleted mapbox geocoder

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,33 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css';
-import Signup from './signup.js';
-import Logout from './signout.js';
-import SignIn from './signin.js';
-import Top from './toptrips.js';
-import Trip from './createatrip.js';
-
+import "./App.css";
+import Signup from "./signup.js";
+import Logout from "./signout.js";
+import SignIn from "./signin.js";
+import Top from "./toptrips.js";
+import Trip from "./createatrip.js";
+import Draggable from "./drag";
 
 function App() {
-
   const domain = /https:\/\/[^/]+/;
-  const basename = process.env.PUBLIC_URL.replace(domain, '');
+  const basename = process.env.PUBLIC_URL.replace(domain, "");
 
   return (
     <BrowserRouter basename={basename}>
-    <div className="navColor">
-    {/* <Nav token={token} /> */}
-    </div>
+      <div className="navColor">{/* <Nav token={token} /> */}</div>
 
       <Routes>
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/login/new" element={<Signup />} />
+        <Route path="/top" element={<Top />} />
+        <Route path="/trip" element={<Trip />} />
+        <Route path="/drag" element={<Draggable />} />
 
-        <Route path="/login"element={<SignIn/>}/>
-        <Route path="/login/new" element={<Signup/>} />
-        <Route path="/top" element={<Top/>} />
-        <Route path="/trip" element={<Trip/>} />
-
-        <Route path="/logout" element={<Logout/>} />
-
+        <Route path="/logout" element={<Logout />} />
       </Routes>
     </BrowserRouter>
   );
