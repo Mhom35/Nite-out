fec7b6a424f140ce48c08d953e6a6254d121280a
Derrick Wan <drrckwan.s@gmail.com>
2022-12-02 05:06:57 +0000

    Finished create trip

--- ghi/src/createatrip.js
+++ ghi/src/createatrip.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from "react-redux";
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import { TextField, Link } from '@mui/material';
@@ -6,34 +8,77 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useState, useRef } from 'react';
 import AddLocation from './Geomap';
-import { useSelector } from 'react-redux';
+import { addLocation } from "./app/locations.js";
 
 const theme = createTheme();
 
 export default function Trip() {
-  const locations = useSelector((state) => state.addLocations.value)
-
+  let locations = useSelector((state) => state.addLocations.value)
+  const [notFinished, setNotFinished] = useState(true)
   const locationsMapRef = useRef();
-
-  // const scrollToSection = (elementRef) => {
-  //   window.scrollTo({
-  //     top: elementRef.current.offsetTop,
-  //     behavior: 'smooth'
-  //   })
-  // }
+  const date = new Date()
+  const dateTime = date.getTime().toString()
 
   const [tripName, setTripName] = useState('');
-  // const [location, setLocation] = useState([]);
   const [description, setDescription] = useState('');
   const [clicked, setClicked] = useState(false)
+  const dispatch = useDispatch();
 
-
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    //const data = new FormData(event.currentTarget);
-  };
+    const data = {
+      "trip_name": tripName,
+      "locations": locations,
+      "description": description,
+      "created_on": dateTime
+    }
+    const tripUrl = "http://localhost:8001/trips"
+    const fetchConfig = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    const tripResponse = await fetch(tripUrl, fetchConfig)
+    if (tripResponse.ok) {
+      const newTrip = await tripResponse.json()
+      const tripId = newTrip.id;
+      locations.forEach(async (bar_id, position) => {
+        const data = {
+          "trip_id": tripId,
+          "bar_id": bar_id,
+          "positions": position
+        }
+        const middleUrl = "http://localhost:8001/middletable"
+        const fetchConfig = {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+        const middleResponse = await fetch(middleUrl, fetchConfig)
+        if (middleResponse.ok) {
+          return true
+        }
+      });
+      clearState()
+    }
+  }
+  const clearState = () => {
+    setTripName("")
+    setDescription("")
+    dispatch(addLocation([]))
+  }
+
+  useEffect(() => {
+    if (locations.length > 0) {
+      setNotFinished(false);
+      setClicked(false)
+    }
+  }, [locations])
 
   return (
     <ThemeProvider theme={theme}>
@@ -42,36 +87,25 @@ export default function Trip() {
         <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', }} >
           <Typography component="h1" variant="h5"> Create a Trip </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField onChange={(event) => setTripName(event.target.value)} value={tripName} margin="normal" fullWidth id="" label="Trip Name" name="" />
+            <TextField required onChange={(event) => setTripName(event.target.value)} value={tripName} margin="normal" fullWidth id="" label="Trip Name" name="" />
             <Link to="#yelpMap">
               <Button type="button" fullWidth variant="outlined" onClick={(event) => setClicked(true)} sx={{ mt: 3, mb: 2 }}> Add Locations </Button>
             </Link>
-            <TextField margin="normal" fullWidth id="outlined-multiline-static" multiline label="Description" name="" rows={3} value={description} onChange={(event) => setDescription(event.target.value)} />
-            {(!clicked && (locations.length > 1)) && (
+            <TextField required margin="normal" fullWidth id="outlined-multiline-static" multiline label="Description" name="" rows={3} value={description} onChange={(event) => setDescription(event.target.value)} />
+            {(!clicked && (locations.length > 0)) && (
               <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}> Submit </Button>
             )}
-            <Button type="button" fullWidth variant="outlined" onClick={(event) => setClicked(false)} sx={{ mt: 3, mb: 2 }}> Finished Adding Locations </Button>
           </Box>
 
-          {clicked && (
+          {(clicked && notFinished) && (
             <>
               <div ref={locationsMapRef}>
-                <AddLocation />
+                <AddLocation key="234" />
               </div>
             </>
           )}
-          {locations?.map((location) => (
-            (
-              <>
-                <div>
-                  <p>{location}</p>
-                </div>
-              </>
-            )
-          ))}
 
         </Box>
-
       </Container>
     </ThemeProvider>
   );
